e8190b1b34f8b77b7f517d97cd431cff1a851856
Joseph Golden <omniint2@protonmail.com>
2020-03-12 12:12:33 -0700

    Declared variables for various exceptions.

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,9 @@ var numInput = function(number) {
     alert("Number must not be negative.");
   }
 }
+
+function loopThroughInput (numInput) {
+  var result = [];
+  var beep = "Beep";
+  var boop = "Boop";
+  var neighbor = "Won't you be my neighbor?";
