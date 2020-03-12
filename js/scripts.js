350c3bb9a21165c2055228fdde5eef5c9b123b72
Joseph Golden <omniint2@protonmail.com>
2020-03-12 12:21:43 -0700

    Added conditionals for each exceptional varying on the user's number input.

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,17 @@ function loopThroughInput (numInput) {
   var beep = "Beep";
   var boop = "Boop";
   var neighbor = "Won't you be my neighbor?";
+  for (var i = 0; i <= numInput; i++) {
+    var numString = numInput.toString();
+    if (i % 3 === 0 && i > 0) {
+      result.push(neighbor);
+    } else if (numString.includes("1")) {
+      result.push(boop);
+    } else if (numString.includes("0")) {
+      result.push(beep);
+    } else {
+      result.push(i.toString());
+    }
+  }
+  return result;
+}
